a58e636d90c373e6c2194b7291da3488d3f5f4bb
JoshMH-91 <138040193+JoshMH-91@users.noreply.github.com>
2023-07-01 11:08:06 -0500

    Add files via upload

--- StoneMCUtoHMI/StoneMCUtoHMI.h
+++ StoneMCUtoHMI/StoneMCUtoHMI.h
@@ -18,20 +18,18 @@ public:
   */
 
   template <typename T>
-String update_label(const String& label, const String& type, const T& value) {
-    String labelCode = "ST<{\"cmd_code\":\"set_" + type + "\",\"type\":\"label\",\"widget\":\"";
-    labelCode += label;
-
+  String update_label(const String& label, const String& type, const T& value) {
+    String updateLabel = "ST<{\"cmd_code\":\"set_" + type + "\",\"type\":\"label\",\"widget\":\"";
+    updateLabel += label;
     if (type == "text") {
-      labelCode += "\",\"text\":\"" + String(value) + "\"";
+      updateLabel += "\",\"text\":\"" + String(value) + "\"";
     }
     else if (type == "value") {
-      labelCode += "\",\"value\":" + String(value);
+      updateLabel += "\",\"value\":" + String(value);
     }
-
-    labelCode += "}>ET";
-    return labelCode;
-}
+    updateLabel += "}>ET";
+    return updateLabel;
+  } 
 
   /**
   * This function sets the text of a multi-line edit widget on the HMI.
@@ -40,17 +38,17 @@ String update_label(const String& label, const String& type, const T& value) {
   * @param text The new text to set.
   *
   * @example
-  * set_mledit_text("MyWidget", "Example text");
+  * update_mledit_text("MyWidget", "Example text");
   */
 
   template <typename T>
-  String set_mledit_text(const String& widget, const T& text) {
-    String setMlEdit = "ST<{\"cmd_code\":\"set_text\",\"type\":\"mledit\",\"widget\":\"";
-    setMlEdit += widget;
-    setMlEdit += "\",\"text\":\"";
-    setMlEdit += String(text);
-    setMlEdit += "\"}>ET";
-    return setMlEdit;
+  String update_mledit_text(const String& widget, const T& text) {
+    String updateMlEdit = "ST<{\"cmd_code\":\"set_text\",\"type\":\"mledit\",\"widget\":\"";
+    updateMlEdit += widget;
+    updateMlEdit += "\",\"text\":\"";
+    updateMlEdit += String(text);
+    updateMlEdit += "\"}>ET";
+    return updateMlEdit;
   }
 
   /**
@@ -79,15 +77,15 @@ String update_label(const String& label, const String& type, const T& value) {
   * @param value The new brightness level.
   *
   * @example
-  * set_brightness(50); // Set maximum brightness
+  * update_brightness(50); // Set maximum brightness
   */
 
   template <typename T>
-  String set_brightness(const T& value) {
-    String setBrightness = "ST<{\"cmd_code\":\"set_brightness\",\"type\":\"system\",\"brightness\":";
-    setBrightness += String(value);
-    setBrightness += "}>ET";
-    return setBrightness;
+  String update_brightness(const T& value) {
+    String updateBrightness = "ST<{\"cmd_code\":\"set_brightness\",\"type\":\"system\",\"brightness\":";
+    updateBrightness += String(value);
+    updateBrightness += "}>ET";
+    return updateBrightness;
   }
 
   /**
@@ -97,28 +95,28 @@ String update_label(const String& label, const String& type, const T& value) {
   * @param value The new volume level, or true/false for the "set_mute" command.
   *
   * @example
-  * set_volume("set_vol", 20); // Set volume level to 20
-  * set_volume("set_mute", true); // Mute the system
+  * update_volume("set_vol", 20); // Set volume level to 20
+  * update_volume("set_mute", true); // Mute the system
   */
 
   template <typename T>
-  String set_volume(const char* command, const T& value) {
-    String setVolume;
+  String update_volume(const char* command, const T& value) {
+    String updateVolume;
 
     if (strcmp(command, "set_vol") == 0) {
-      setVolume = "ST<{\"cmd_code\":\"set_vol\",\"type\":\"system\",\"vol\":" + String(value) + "}>ET";
+      updateVolume = "ST<{\"cmd_code\":\"set_vol\",\"type\":\"system\",\"vol\":" + String(value) + "}>ET";
     }
     else if (strcmp(command, "set_vol_inc") == 0) {
-      setVolume = "ST<{\"cmd_code\":\"set_vol_inc\",\"type\":\"system\",\"step\":" + String(value) + "}>ET";
+      updateVolume = "ST<{\"cmd_code\":\"set_vol_inc\",\"type\":\"system\",\"step\":" + String(value) + "}>ET";
     }
     else if (strcmp(command, "set_vol_dec") == 0) {
-      setVolume = "ST<{\"cmd_code\":\"set_vol_dec\",\"type\":\"system\",\"step\":" + String(value) + "}>ET";
+      updateVolume = "ST<{\"cmd_code\":\"set_vol_dec\",\"type\":\"system\",\"step\":" + String(value) + "}>ET";
     }
     else if (strcmp(command, "set_mute") == 0) {
-      setVolume = "ST<{\"cmd_code\":\"set_mute\",\"type\":\"system\",\"mute\":" + String(value ? "true" : "false") + "}>ET";
+      updateVolume = "ST<{\"cmd_code\":\"set_mute\",\"type\":\"system\",\"mute\":" + String(value ? "true" : "false") + "}>ET";
     }
 
-    return setVolume;
+    return updateVolume;
   }
 
   /**
@@ -156,15 +154,26 @@ String update_label(const String& label, const String& type, const T& value) {
   * @note The widget name should match the name of the radio button widget on the HMI.
   */
 
-  String radio_button(const String& widget, bool state){
+  String update_radio_button(const String& widget, bool state){
     String command = "<ST{\"cmd_code\":\"set_value\",\"type\":\"radio_button\",\"widget\":\"";
     command += widget;
-    command += "\",";
+    command += "\",\"value\":";
     command += state;
     command += "}>ET";
     
     return command;
   }
+
+  String update_switch(const String& widget, bool state){
+    String command = <"ST{\"cmd_code\":\"set_value\",\"type\":\"switch\",\"widget\":\"";
+    command += widget;
+    command += "\",\"value\":"
+    command += state;
+    command += "}>ET";
+
+    return command;
+  }
+
 };
 
 #endif
